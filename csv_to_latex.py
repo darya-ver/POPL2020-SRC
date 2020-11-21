27d90388985665025cbb9795644b7ee7b50019dd
Darya Verzhbinsky <darya99@gmail.com>
2020-11-20 23:17:53 -0800

    ready for daniel to read

--- csv_to_latex.py
+++ csv_to_latex.py
@@ -4,7 +4,7 @@ HEADER = "\\begin{table*}\n" + \
   "\\tiny\n" + \
   "\\caption{\\textsc{Tygar} vs. \\textsc{Petsy}}\n" + \
   "\\label{tab:results}\n" + \
-  "\\begin{tabular}{c c c | r | r | r}\n"
+  "\\begin{tabular}{|l |l l| r | r |}\n"
 
 FOOTER = "\end{tabular}\n" + \
   "\end{table*}\n"
@@ -15,16 +15,17 @@ FOOTER = "\end{tabular}\n" + \
 #     else:
 #         return '{0:0.2f}'.format(t)
 
-# TODO: format percentage and get corret set of tests
-
 def write_latex():
     '''Generate Latex table from the results dictionary'''
         
     with open("results.tex", 'w') as outfile:
       outfile.write(HEADER)
       outfile.write ('\\hline\n')
-      outfile.write("N & Name & Query   &  Time (s): \\textsc{Tygar}	& Time (s): \\textsc{Petsy} & Speedup \\\\ \n")
+      # outfile.write("N & Name & Query   &  Time (s): \\textsc{Tygar}	& Time (s): \\textsc{Petsy} & \\textsc{Petsy} speedup over \\textsc{Tygar} \\\\ \n")
+      # outfile.write("N & Name & Query   &  Time (s): \\textsc{Tygar}	& Time (s): \\textsc{Petsy} \\\\ \n")
+      outfile.write("N & Name & Query   &  \\textsc{Tygar}	& \\textsc{Petsy} \\\\ \n")
       outfile.write ('\\hline\n')
+      
       with open('results.csv') as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
         line_count = 0
@@ -35,15 +36,16 @@ def write_latex():
 
           name = row[0]
           query = row[1]
-          tygar_time = row[2]
-          our_time = row[3]
-          speedup = row[4]
+          tygar_time = row[2] + " s"
+          our_time = row[3] + " s"
+          # speedup = row[5][:-2] + "\%"
 
-          table_row = str(line_count - 2) + " & " + name + " & " + \
-            query + " & " + \
-            tygar_time + " & " + \
-            our_time + " & " + \
-            speedup + " \\\\ \n"
+          table_row = str(line_count - 2) + " & " + name + \
+            " & " + query + \
+            " & " + tygar_time + \
+            " & " + our_time + \
+            " \\\\ \n"
+            # " & " + speedup + \
           
           if(line_count < 23):
             outfile.write(table_row)
